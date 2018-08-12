974d540a34bedd5bdaf70669d44d1171f760c44b
Scar Wu <xneriscool@gmail.com>
2018-08-12 21:17:42 +0800

    1. add store classes: db, cache

---
+++ src/Oni/Web/Store/Cache.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Cache Store
+ *
+ * @package     Oni
+ * @author      Scar Wu
+ * @copyright   Copyright (c) Scar Wu (https://scar.tw)
+ * @link        https://github.com/scarwu/Oni
+ */
+
+namespace Oni\Web;
+
+use Exception;
+use Memcached;
+use Oni\Basic;
+
+class Cache extends Basic
+{
+    /**
+     * @var object
+     */
+    private static $_instance = null;
+
+    /**
+     * Construct
+     *
+     * This function is private, so this class is singleton pattern
+     */
+    private function __construct() {}
+
+    /**
+     * Initialize
+     */
+    public static function init($config = null)
+    {
+        if (null === self::$_instance) {
+            self::$_instance = Memcached();
+        }
+
+        return self::$_instance;
+    }
+}
---
+++ src/Oni/Web/Store/Database.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Database Store
+ *
+ * @package     Oni
+ * @author      Scar Wu
+ * @copyright   Copyright (c) Scar Wu (https://scar.tw)
+ * @link        https://github.com/scarwu/Oni
+ */
+
+namespace Oni\Web;
+
+use Exception;
+use Oni\Basic;
+
+class Database extends Basic
+{
+    /**
+     * @var object
+     */
+    private static $_instance = null;
+
+    /**
+     * Construct
+     *
+     * This function is private, so this class is singleton pattern
+     */
+    private function __construct() {}
+
+    /**
+     * Initialize
+     */
+    public static function init($config = [])
+    {
+        if (null === self::$_instance) {
+
+            // config: host, port, name, user, pass
+            $conn = new PDO("mysql:host={$config['host']};port={$config['port']};dbname={$config['name']}", $config['user'], $config['pass']);
+            $conn->query("SET NAMES 'utf8'");
+            $conn->query("SET CHARACTER_SET_CLIENT=utf8");
+            $conn->query("SET CHARACTER_SET_RESULTS=utf8");
+
+            self::$_instance = $conn;
+        }
+
+        return self::$_instance;
+    }
+}
